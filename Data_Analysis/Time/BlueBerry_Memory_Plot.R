55f80aeae5c423b70cb52d25954c9cd01a0c5b6e
nearinj <jc235601@dal.ca>
2017-12-21 19:14:40 -0500

    Added new scripts to generate time and memory plots for the blueberry data

---
+++ Data_Analysis/Time/BlueBerry_Memory_Plot.R
@@ -0,0 +1,56 @@
+#used to plot memory usage foor each pipeline
+
+library(ggplot2)
+
+setwd("~/GitHub_Repos/Denoiser Comparison/Data_Analysis/Time/")
+
+MasterTable <- read.table("Blueberry_time_data.tsv", sep="\t", header=T)
+
+#get Deblur memory
+deblur_memory <- MasterTable[grep("Deblur", MasterTable$Pipe),c("Filt", "TimeCMD","Memory.kb.")]
+deblur_memory$Pipe <- "Deblur"
+#Remove 40k reads as the read depth was deep enough to support this rarefaction
+deblur_memory <- deblur_times[-5,]
+
+
+#get DADA2 memory
+
+MemoryDataInference <- MasterTable[which(MasterTable$TimeCMD == "timeI"),]
+MemoryDataChimera <- MasterTable[which(MasterTable$TimeCMD == "timeCT"),]
+rownames(MemoryDataInference) <- paste(MemoryDataInference$Filt, MemoryDataInference$Pipe, sep="_")
+rownames(MemoryDataChimera) <- paste(MemoryDataChimera$Filt, MemoryDataChimera$Pipe, sep="_")
+dada2_memory <- MemoryDataInference
+
+dada2_memory <- dada2_memory[,-c(4,5)]
+dada2_memory <- dada2_memory[-5,]
+dada2_memory$Pipe <- "DADA2"
+
+#get UNOISE memory
+
+
+MemoryDataD <- MasterTable[which(MasterTable$TimeCMD == "Dtime"),]
+MemoryDataUni <- MasterTable[which(MasterTable$TimeCMD == "Unitime"),]
+rownames(MemoryDataD) <- paste(MemoryDataD$Filt, MemoryDataD$Pipe, sep="_")
+rownames(MemoryDataUni) <- paste(MemoryDataUni$Filt, MemoryDataUni$Pipe, sep="_")
+Unoise_Memory <- MemoryDataUni
+Unoise_Memory <- Unoise_Memory[,-c(4,5)]
+Unoise_Memory <- Unoise_Memory[-5,]
+Unoise_Memory$Pipe <- "UNOISE3"
+
+#Make one large table
+recombined_Memory <- rbind(deblur_memory, dada2_memory, Unoise_Memory)
+colnames(recombined_Memory)[4] <- "Pipeline"
+
+#set the bar order
+orders <- c("UNOISE3 325k", "UNOISE3 648k", "UNOISE3 1287k", "UNOISE3 1926k", 
+            "Deblur 325k", "Deblur 648k", "Deblur 1287k", "Deblur 1926k",
+            "DADA2 325k", "DADA2 648k", "DADA2 1287k", "DADA2 1926k")
+#rename column to be used as labels in graph
+recombined_Memory$Filt <- paste(recombined_Memory$Pipe, c("325k", "648k", "1287k", "1926k"), sep = " ")
+group.colors <- c(Deblur = "#333BFF", DADA2 = "#CC6600", UNOISE3 = "#9633FF")
+recombined_Memory$Filt <- factor(recombined_Memory$Filt, levels = orders)
+
+#make the plot
+Memoryplot <- ggplot(recombined_Memory, aes(x= Filt, y=Memory.kb., fill=Pipeline)) + geom_bar(stat = "identity") + scale_fill_manual(values=group.colors) +
+  xlab("") + theme(axis.text = element_text(size=10)) + ylab("Memory used in Kb") + scale_y_continuous(breaks=c(1000000,2000000,3000000,4000000,5000000))
+Memoryplot
---
+++ Data_Analysis/Time/Time_Data_for_Blueberry.R
@@ -0,0 +1,55 @@
+library(ggplot2)
+
+setwd("~/GitHub_Repos/Denoiser Comparison/Data_Analysis/Time/")
+
+MasterTable <- read.table("Blueberry_time_data.tsv", sep="\t", header=T)
+
+#get Deblur times
+deblur_times <- MasterTable[grep("Deblur", MasterTable$Pipe),c("Filt", "TimeCMD","User", "Sys")]
+deblur_times$Pipe <- "Deblur"
+deblur_times <- deblur_times[,-4]
+deblur_times <- deblur_times[-5,]
+
+#Combined Dada2
+timeDataInference <- MasterTable[which(MasterTable$TimeCMD == "timeI"),]
+timeDataChimera <- MasterTable[which(MasterTable$TimeCMD == "timeCT"),]
+rownames(timeDataInference) <- paste(timeDataInference$Filt, timeDataInference$Pipe, sep="_")
+rownames(timeDataChimera) <- paste(timeDataChimera$Filt, timeDataChimera$Pipe, sep="_")
+dada2_time <- timeDataInference
+dada2_time$User <- timeDataInference$User + timeDataChimera[rownames(timeDataInference), "User"]
+dada2_time <- dada2_time[,-c(5,6)]
+dada2_time <- dada2_time[-5,]
+dada2_time$Pipe <- "DADA2"
+#Combined UnoiseTimes
+
+timeDataD <- MasterTable[which(MasterTable$TimeCMD == "Dtime"),]
+timeDataUni <- MasterTable[which(MasterTable$TimeCMD == "Unitime"),]
+rownames(timeDataD) <- paste(timeDataD$Filt, timeDataD$Pipe, sep="_")
+rownames(timeDataUni) <- paste(timeDataUni$Filt, timeDataUni$Pipe, sep="_")
+Unoise_time <- timeDataD
+Unoise_time$User <- timeDataD$User + timeDataUni[rownames(timeDataD), "User"]
+Unoise_time <- Unoise_time[,-c(5,6)]
+Unoise_time <- Unoise_time[-5,]
+Unoise_time$Pipe <- "UNOISE3"
+
+recombined_time <- rbind(deblur_times, dada2_time, Unoise_time)
+colnames(recombined_time)[4] <- "Pipeline"
+
+orders <- c("UNOISE3 325k", "UNOISE3 648k", "UNOISE3 1287k", "UNOISE3 1926k", 
+            "Deblur 325k", "Deblur 648k", "Deblur 1287k", "Deblur 1926k",
+            "DADA2 325k", "DADA2 648k", "DADA2 1287k", "DADA2 1926k")
+
+
+
+recombined_time$Filt <- paste(recombined_time$Pipe, c("325k", "648k", "1287k", "1926k"), sep = " ")
+
+
+#barplot(log10(recombined_time$User), names.arg = argnames, las=2, ylab = "log10(User time in s)", main = "Speed of the Different Pipelines", )
+
+group.colors <- c(Deblur = "#333BFF", DADA2 = "#CC6600", UNOISE3 = "#9633FF")
+
+recombined_time$Filt <- factor(recombined_time$Filt, levels = orders)
+
+timeplot <- ggplot(recombined_time, aes(x= Filt, y=log10(User), fill=Pipeline)) + geom_bar(stat = "identity") + scale_fill_manual(values=group.colors) +
+  xlab("") + theme(axis.text = element_text(size=10)) + ylab("User Time in Seconds") + scale_y_continuous(breaks=c(0,1,2,3,4,5), labels=c("0","10","100","1000","10000","100000"))
+timeplot
